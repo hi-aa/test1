63666f41d6a6a0d46fece2dce4e1f51b878aa908
reum <ost.jj1201@gmail.com>
2021-09-05 17:20:04 +0900

    강의 22강

--- lib/main.dart
+++ lib/main.dart
@@ -22,42 +22,45 @@ class MyPage extends StatelessWidget {
         centerTitle: true,
       ),
       body: Center(
-       child: Center(
-         // child: Column(
-         child: Row(
-           // verticalDirection: VerticalDirection.up,
-           // mainAxisAlignment: MainAxisAlignment.center,
-           // mainAxisSize: MainAxisSize.min,
-           // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-           // crossAxisAlignment: CrossAxisAlignment.end,
-           crossAxisAlignment: CrossAxisAlignment.stretch,
-           children: [
-             Container(
-               width: 100,
-               color: Colors.white,
-               child: Text('Container 1'),
-             ),
-              SizedBox(
-                width: 30.0,
-              ),
-              Container(
-                width: 100,
-                color: Colors.blue,
-                child: Text('Container 2'),
-              ),
-              Container(
-                width: 100,
-                color: Colors.red,
-                child: Text('Container 3'),
-              ),
-              // Container( // invisible container
-              //   width: double.infinity,
-              //   height: 20,
-              // )
-           ],
-         ),
-       ),
+        child: RaisedButton(
+          child: Text('Go to second page'),
+          onPressed: () {
+            Navigator.push(context, MaterialPageRoute(
+                builder: (context) => SecondPage())
+
+            );
+//           ));
+          },
+        ),
       ),
     );
+  }
+}
+
+class SecondPage extends StatelessWidget{
+  @override
+  Widget build(BuildContext ctx) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Second PAge'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: RaisedButton(
+          child: Text('Go to second page'),
+          onPressed: () {
+            Navigator.pop(ctx);
+
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class tt extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
