38675cd8aec4ba2fef6766b622bf3d9079633e47
reum <ost.jj1201@gmail.com>
2021-09-12 17:22:12 +0900

    강의 26강 2.0 Snackbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Appbar',
+      theme: ThemeData(
+        primarySwatch: Colors.blue
+      ),
       home: MyPage(),
     );
   }
@@ -18,49 +21,104 @@ class MyPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Toast message'),
-        centerTitle: true,
+        title: Text('Scaffold Messenger'),
       ),
-      body: Center(
-        child: RaisedButton(
-          child: Text('Go to second page'),
-          onPressed: () {
-            Navigator.push(context, MaterialPageRoute(
-                builder: (context) => SecondPage())
-
-            );
-//           ));
-          },
-        ),
-      ),
-    );
-  }
-}
-
-class SecondPage extends StatelessWidget{
-  @override
-  Widget build(BuildContext ctx) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Second PAge'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: RaisedButton(
-          child: Text('Go to second page'),
-          onPressed: () {
-            Navigator.pop(ctx);
-
-          },
-        ),
+      body: HomeBody(),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.thumb_up),
+        onPressed: () {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text('Like a new Snack Bar!'),
+              duration: Duration(seconds: 5),
+              action: SnackBarAction(
+                label: 'Undo',
+                onPressed: () {
+                  // 페이지 이동
+                  Navigator.push(context
+                      , MaterialPageRoute(builder: (context) => ThirdPage())
+                  );
+                },
+              ),
+            ),
+          );
+        },
       ),
     );
   }
 }
 
-class tt extends StatelessWidget {
+class HomeBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: ElevatedButton(
+        child: Text('Go Second Page'),
+        onPressed: () {
+          Navigator.push(context
+              , MaterialPageRoute(builder: (context) => SecondPage())
+          );
+        },
+      ),
+    );
+  }
+}
+
+class SecondPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Second Page'),
+      ),
+      body: Center(
+        child: Text(
+          '좋아요가 추가되었습니다.',
+          style: TextStyle(
+            fontSize: 20.0,
+            color: Colors.redAccent
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ThirdPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ScaffoldMessenger( // root scaffold가 아님
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Third Page'),
+        ),
+        body: Builder( // 다시 builder를 추가해서
+          builder: (context) { // context를 찾아줌?
+            return Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    '좋아요를 취소하시겠습니까?.',
+                    style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.redAccent
+                    ),
+                  ),
+                  ElevatedButton(
+                      onPressed: () {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(content: Text('좋아요가 취소되었습니다.'),
+                              duration: Duration(seconds: 3),
+                            )
+
+                        );
+                      }, child: Text('취소하기'))
+                ],
+              ),
+            );
+          }
+        ),
+      ),
+    );
   }
 }
