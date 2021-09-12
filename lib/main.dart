713c1044c48f5178889e59621504ca9864ecc1d5
reum <ost.jj1201@gmail.com>
2021-09-12 17:44:49 +0900

    강의 27강 2.0 버튼

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue
       ),
-      home: MyPage(),
+//      home: MyPage(),
+     home: MyButtons(),
     );
   }
 }
@@ -117,6 +118,86 @@ class ThirdPage extends StatelessWidget {
               ),
             );
           }
+        ),
+      ),
+    );
+  }
+}
+
+class MyButtons extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Buttons'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextButton(
+                onPressed: () {
+                  print('click text button');
+                }, child: Text(
+              'Text Button',
+              style: TextStyle(
+                fontSize: 20.0
+              ),
+            ),
+              style: TextButton.styleFrom(
+                primary: Colors.red,
+                // backgroundColor: Colors.blue
+              ),
+            ),
+            ElevatedButton(
+                onPressed: null, // 버튼 비활성화
+              child: Text('Elevated button'),
+              style: ElevatedButton.styleFrom(
+                primary: Colors.orangeAccent, // 배경색
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0)
+
+                ),
+                onSurface: Colors.pink
+              ),
+            ),
+            OutlineButton(
+                onPressed: () {
+                  print('Outlined');
+              },
+              child: Text('Outlined button'),
+              borderSide: BorderSide(
+                color: Colors.black87,
+                width: 2.0
+              ),
+            ),
+            TextButton.icon(
+                onPressed: () {
+
+                  },
+                icon: Icon(
+                  Icons.home,
+                ),
+                label: Text('go to home')
+              ),
+            ButtonBar( // 정렬
+              alignment: MainAxisAlignment.center,
+              buttonPadding: EdgeInsets.all(20), // 버튼 패딩
+              children: [
+                TextButton(
+                    onPressed: () {
+                      },
+                    child: Text('Butt1')
+                ),
+                TextButton(
+                    onPressed: () {
+                      },
+                    child: Text('Butt2')
+                ),
+              ],
+            )
+          ],
         ),
       ),
     );
